c7bff6d4018f29daf8bfbf8380caf67efa857fdd
Brooke Finister <brookeafinister@gmail.com>
2022-11-23 20:49:45 -0800

    created dog class

---
+++ my_dogs.py
@@ -0,0 +1,18 @@
+'''Docstring Content'''
+
+#import libraries
+import dog
+
+
+my_dog = dog.Dog("Rex", "SuperDog")
+my_dog.bark()
+my_other_dog =dog.Dog("Annie", "SuperDog")
+print(my_other_dog.name)
+
+dog1 = dog.Dog("Bowser", "Bull Dog")
+dog1.sit()
+dog2 = dog.Dog("Taco", "Chihuahua")
+dog2.rollover()
+dog3 = dog.Dog("Levi", "Schnauzer")
+dog3.bark()
+
